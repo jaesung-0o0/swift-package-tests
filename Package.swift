83de03222be6697541c99fb34600e9ce9a6bc4ea
Jaesung Lee <jaesung.lee@sendbird.com>
2023-01-02 14:19:57 +0900

    [버전] 1.0.1

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPackageTests",
-    platforms: [.iOS("v16")],
+    platforms: [
+        .iOS(.v16),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
