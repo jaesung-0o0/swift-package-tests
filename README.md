26026b9b22b2b8d44f79ec3694afc246ff0919b4
Jaesung Lee <jaesung.lee@sendbird.com>
2023-01-02 14:18:11 +0900

    [버전] 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftPackageTests
+
+A description of this package.
---
+++ Sources/SwiftPackageTests/SwiftPackageTests.swift
@@ -0,0 +1,8 @@
+public struct SwiftPackageTests {
+    public private(set) var text = "Hello, World!"
+
+    public static var version: String { "v1" }
+    
+    public init() {
+    }
+}
